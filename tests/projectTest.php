dc437d704419b94da036ed4ecf1f9cc1cb7e4c17
Bruno Fontes <developer@brunofontes.net>
2019-06-27 20:21:43 -0300

    Fixing a project Test
    The test was to make sure it would return a error on No token, but it
    was providing a token. More than that, it were a simple and direct check
    if it were throwing an error.
    
    Now it is checking for the specific Missing Access Token error message.

--- tests/projectTest.php
+++ tests/projectTest.php
@@ -8,10 +8,11 @@ final class ProjectTest extends TestCase
 {
     public function testNoTokenShouldThrowError()
     {
-        $api = new Memsource('fakeToken');
-        $this->expectException(\Exception::class);
+        $api = new Memsource();
+        $this->expectExceptionMessage('Missing Access Token');
         $api->project()->list();
     }
+
     public function testInvalidProjectUidShouldThrowError()
     {
         $api = new Memsource('fakeToken');
